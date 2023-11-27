8b7d67197d08eb3b0e4e6d977965252a425f7c8c
Sachin Uthpala <140322238+SachinUthpala@users.noreply.github.com>
2023-11-27 23:58:08 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,549 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--upper image-->
+    <link rel="icon" type="image/svg+xml" href="/imgs/Web-img/pre.jpeg" />
+    <!--style sheets-->
+    <link rel="stylesheet" href="./Styles/index.css" />
+    <script
+      src="https://kit.fontawesome.com/f096ac9546.js"
+      crossorigin="anonymous"
+    ></script>
+    <title>Foodies</title>
+    <!--google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap"
+      rel="stylesheet"
+    />
+
+    <script src="./script/index.js"></script>
+  </head>
+  <body>
+    <div class="sec01 flexBetween">
+      <div class="left flexGap">
+        <div class="child01 flexGap">
+          <i class="fa-solid fa-phone-volume" style="color: #0d0d0d"></i>
+          <div>
+            <span class="secondaryText"> 0762712200 </span><br />
+            <span class="secondaryText"> CONTACT US </span>
+          </div>
+        </div>
+        <div class="child02 flexGap">
+          <i class="fa-solid fa-car" style="color: #000000"></i>
+          <div>
+            <span class="secondaryText"> PERSONALISED </span><br />
+            <span class="secondaryText"> DELIVERY </span>
+          </div>
+        </div>
+      </div>
+      <div class="middle">
+        <span class="primaryText"> Foodies Mania </span><br />
+        <span class="secondaryText"> Colombo , Sri Lanka </span>
+      </div>
+      <div class="right flexGap">
+        <i class="fa-regular fa-clock"></i>
+        <div>
+          <span class="secondaryText"> DAILY DELIVERY FROM (MON - SUN) </span
+          ><br />
+          <span class="secondaryText"> 10.30 AM - 10.30 PM </span>
+        </div>
+      </div>
+    </div>
+
+    <!--navigation section-->
+    <nav>
+      <div class="nav1">
+        <ul class="flexGap menues">
+          <li><a href="">Home</a></li>
+          <li><a href="#starters">Starters</a></li>
+          <li><a href="#mains">Mains</a></li>
+          <li><a href="">Desserts</a></li>
+          <li><a href="">Packages</a></li>
+        </ul>
+        <i
+          class="fa-solid fa-bars"
+          id="menuIcon"
+          style="color: #000000"
+          onclick="openMenu()"
+        ></i>
+      </div>
+      <div class="nav2 flexGap">
+        <i class="fa-regular fa-user" style="color: #000000"></i>
+        <span class="login"> LOGIN / SIGN UP </span>
+      </div>
+    </nav>
+
+    <div class="sideMenu" id="sideMenu">
+      <ul>
+        <li>
+          <i
+            class="fa-solid fa-x"
+            style="color: #000000; margin: 0 90px"
+            onclick="closeMenu()"
+          ></i>
+        </li>
+        <li><a href="">Home</a></li>
+        <li><a href="#starters" onclick="closeMenu()">Starters</a></li>
+        <li><a href="#mains" onclick="closeMenu()">Mains</a></li>
+        <li><a href="">Desserts</a></li>
+        <li><a href="">Packages</a></li>
+      </ul>
+    </div>
+    <!--end of navigation section-->
+
+    <!--seconf section-->
+    <div class="sec02">
+      <br /><br /><br /><br /><br />
+      <span class="primaryText maintext"> Well Come Foodies </span><br /><br />
+      <span class="secondaryText">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
+        earum?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
+        earum? Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit
+        amet consectetur, Lorem ipsum dolor sit amet consectetur,<br />
+      </span>
+    </div>
+    <!--end os 2nd section-->
+
+    <!--3 section section-->
+    <div class="sec03" id="starters">
+      <div class="headdings">
+        <span class="primaryText"> Starters </span><br /><br />
+        <span class="secondaryText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, vel!
+        </span>
+      </div>
+      <br /><br />
+      <div class="items">
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--3 end section section-->
+
+    <!--4 section section-->
+    <div class="sec03" id="mains">
+      <div class="headdings">
+        <span class="primaryText"> Mains </span><br /><br />
+        <span class="secondaryText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, vel!
+        </span>
+      </div>
+      <div class="items">
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--4 end section section-->
+
+    <!--section 5-->
+    <div class="sec03" id="mains">
+      <div class="headdings">
+        <span class="primaryText"> Mains </span><br /><br />
+        <span class="secondaryText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, vel!
+        </span>
+      </div>
+      <div class="items">
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="product">
+          <div class="product-img">
+            <img src="./imgs/Web-img/BackGround.jpg" alt="" />
+          </div>
+          <div class="product-name">
+            <span class="secondaryText"> Food Item </span><br /><br />
+            <span style="line-height: 25px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+              expedita.
+            </span>
+          </div>
+          <div class="addToCart">
+            <button type="button">
+              ADD TO CART - <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--end of section 5-->
+
+    <!--footer-->
+    <div class="footer flexBetween">
+        <div class="left">
+            <span class="primaryText">
+                Foodies
+            </span><br>
+            <span class="secondaryText">
+                Colombo , Sri Lanka
+            </span>
+        </div>
+        <div class="middle">
+            <ul>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">Terms & Conditions</a></li>
+                <li><a href="">Reviews & Ratings</a></li>
+            </ul>
+        </div>
+        <div class="middle2">
+            <span class="secondaryText">
+                Follow Us
+            </span>
+            <br><br>
+            <div class="flexGap icon-group">
+                <i class="fa-brands fa-facebook-f" style="color: #0a0a0a;"></i>
+                <i class="fa-brands fa-instagram" style="color: #000000;"></i>
+                <i class="fa-brands fa-twitter" style="color: #090a0b;"></i>
+            </div>
+        </div>
+        <div class="right">
+            <span class="secondaryText">
+                Customer Care
+            </span><br>
+            <span class="secondaryText">
+                076 27 12200
+            </span><br><br>
+            <span class="secondaryText">
+                Email 
+            </span><br>
+            <span class="secondaryText">
+                foodies @ yahoo . com
+            </span>
+        </div>
+    </div>
+    <!--end of the footer-->
+  </body>
+</html>
---
+++ script/index.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("sideMenu").style.right = "0";
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").style.right = "-500px"; 
+  
+}
